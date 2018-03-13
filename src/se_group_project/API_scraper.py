ebb5df03a3dfed10cdd091029acc1b2a5fe14dfd
Robert De Bhal <robert.de-bhal@ucdconnect.ie>
2018-03-13 13:44:09 +0000

    Create script to update static data in RDS

--- src/se_group_project/API_scraper.py
+++ src/se_group_project/API_scraper.py
@@ -2,6 +2,7 @@ import json
 import requests
 import pandas as pd
 import sqlalchemy
+from time import sleep
 
 # source https://www.digitalocean.com/community/tutorials/how-to-use-web-apis-in-python-3
 api_token = '7e813fe2e25367cd1aa3c4403c764332448fce48' 
@@ -20,18 +21,26 @@ def get_contracts_info():
     else:
         return None
     
-dublin_stations_test = get_contracts_info()
-
 engine = sqlalchemy.create_engine('mysql+pymysql://teamforsoft:whocares1@teamforsoft.ci76dskzcb0m.us-west-2.rds.amazonaws.com:3306/SE_group_project')
 conn = engine.connect()
+while True:
+    dublin_stations_test = get_contracts_info()
+    
+    #chaging positions filed to two separate lat and lng methods
+    for i in range(len(dublin_stations_test)):
+        dublin_stations_test[i]['lat']=dublin_stations_test[i]['position']['lat']
+        dublin_stations_test[i]['lng']=dublin_stations_test[i]['position']['lng']
 
-#converting json to data frame
-df = pd.read_json(json.dumps(dublin_stations_test))
-df.drop('position',1,inplace=True)
+    for i in range(len(dublin_stations_test)):
+        del(dublin_stations_test[i]['position'])
+    
+    #converting json to data frame
+    df = pd.read_json(json.dumps(dublin_stations_test))
+    
+    #appending data frame to SQL table in RDS
+    df.to_sql(name='dynamic',con=conn,if_exists='replace',index=False)
 
-#appending data frame to SQL table in RDS
-df.to_sql(name='stations',con=conn,if_exists='append')
-
-df1 = pd.read_sql_query('SELECT name, available_bikes FROM stations', engine)
-print(df1)
+    df1 = pd.read_sql_query('SELECT name, lat, lng, available_bikes FROM stations WHERE name="BLACKHALL PLACE"', engine)
+    print(df1)
+    sleep(300)
 
