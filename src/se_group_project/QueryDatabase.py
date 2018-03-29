65b50b559c25790ca1ca308d6425b7b709251c83
Orla Gartland <orla.gartland@ucdconnect.ie>
2018-03-29 10:35:44 +0100

    getting flask app to read from json object

--- src/se_group_project/QueryDatabase.py
+++ src/se_group_project/QueryDatabase.py
@@ -3,25 +3,31 @@ import requests
 import pandas as pd
 import sqlalchemy
     
-def getStaticInfo(dublin_stations_test, engine, conn):  
+def getStaticInfo(engine, conn):  
     df_query = pd.read_sql_query('SELECT * FROM static ORDER BY number desc', engine)
-    query_json = df_query.to_json(orient='records')
+    df_dict = df_query.to_dict(orient='records')
+#     print(df_dict)
+    return df_dict
+#     print(df_dict)
+#     with open('newinfo.text', 'w') as outfile:
+#         outfile.write(df_dict)
+#     query_json = df_query.to_json(orient='records')
+#     print(query_json)
+#     df_querydf_query
+    
 
-    with open('templates/static_data.json', 'w') as f:
-        f.write(query_json)
-
-    print(query_json)
+#     print(query_json)
 
     
 def main():
-    from API_scraper import api_token, api_url_base, get_contracts_info
+    from API_scraper import api_token, api_url_base
 
     engine = sqlalchemy.create_engine('mysql+pymysql://teamforsoft:whocares1@teamforsoft.ci76dskzcb0m.us-west-2.rds.amazonaws.com:3306/SE_group_project')
     conn = engine.connect()
     
-    static_test = get_contracts_info()
+#     static_test = get_contracts_info()
     
-    getStaticInfo(static_test, engine, conn)
+    getStaticInfo( engine, conn)
 
 
 if __name__ == "__main__":
--- src/se_group_project/views.py
+++ src/se_group_project/views.py
@@ -2,22 +2,14 @@ from flask import render_template
 from se_group_project import app
 from flask import jsonify
 from flask import json
+import QueryDatabase
+import sqlalchemy
 
-name={'ClarendonRow':{'lat':53.340927,'lng':-6.262501},
-'BlessingtonStreet':{'lat':53.35676899999999,
-'lng':-6.26814},
-'BoltonStreet':{'lat':53.351181999999994,
-'lng':-6.269858999999999},
-'GreekStreet':{'lat':53.346874,'lng':-6.272976},
-'CharlemontStreet':{'lat':53.330662,'lng':-6.260177},
-'ChristchurchPlace':{'lat':53.343368000000005,
-'lng':-6.2701199999999995},
-'HighStreet':{'lat':53.343565000000005,'lng':-6.275071},
-'CustomHouseQuay':{'lat':53.34788399999999,
-'lng':-6.248048000000001},
-'ExchequerStreet':{'lat':53.343033999999996,
-'lng':-6.263578}}
-
+from API_scraper import api_token, api_url_base
+engine = sqlalchemy.create_engine('mysql+pymysql://teamforsoft:whocares1@teamforsoft.ci76dskzcb0m.us-west-2.rds.amazonaws.com:3306/SE_group_project')
+conn = engine.connect()
+name = QueryDatabase.getStaticInfo(engine, conn)
+# print (name)
 @app.route('/')
 def weather(name=name):
     return render_template("weather.html",name=name)
