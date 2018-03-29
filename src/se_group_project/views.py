86cba70681b61f178f8adc09bd9e44dc403c92b9
Orla Gartland <orla.gartland@ucdconnect.ie>
2018-03-29 17:43:48 +0100

    adding test files for individual developers

--- src/se_group_project/views.py
+++ src/se_group_project/views.py
@@ -4,12 +4,26 @@ from flask import jsonify
 from flask import json
 import QueryDatabase
 import sqlalchemy
+import f
 
 from API_scraper import api_token, api_url_base
 engine = sqlalchemy.create_engine('mysql+pymysql://teamforsoft:whocares1@teamforsoft.ci76dskzcb0m.us-west-2.rds.amazonaws.com:3306/SE_group_project')
 conn = engine.connect()
 name = QueryDatabase.getStaticInfo(engine, conn)
 # print (name)
+
+@app.route('/')
+def weather(name=name):
+    return render_template("weather.html",name=name)
+
+@app.route('/fatima')
+def weather(name=f.name):
+    return render_template("weatherf.html",name=name)
+
+@app.route('/')
+def weather(name=name):
+    return render_template("weather.html",name=name)
+
 @app.route('/')
 def weather(name=name):
     return render_template("weather.html",name=name)
