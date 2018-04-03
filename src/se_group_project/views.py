f7bb0c90d3b1192f3fb7b6dcd1a98c8ccc519c26
Orla Gartland <orla.gartland@ucdconnect.ie>
2018-04-03 15:19:30 +0100

    removing 404.html file as causing errors

--- src/se_group_project/views.py
+++ src/se_group_project/views.py
@@ -58,11 +58,11 @@ def JSONo(json=o.json):
     return jsonify(json)
 
 #http://flask.pocoo.org/docs/0.12/patterns/errorpages/
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+#@app.errorhandler(404)
+#def page_not_found(e):
+#    return render_template('404.html'), 404
 
 
-@app.errorhandler(500)
-def internal_error(e):
-    return render_template('500.html'), 500
+#@app.errorhandler(500)
+#def internal_error(e):
+#    return render_template('500.html'), 500
